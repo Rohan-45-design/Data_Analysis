eca9da48aadd88db861e51b43a4d8750226d7a13
Rohan-45-design <149807616+Rohan-45-design@users.noreply.github.com>
2024-08-11 17:58:00 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -10,4 +10,10 @@ df = df.dropna(subset=['S1','S2','S3'],axis=0)
 df = df.replace(np.nan,22)
 df["S3"]+=1000
 df=df.rename(columns={"S3":"S3+1000"})
+print(df["S3+1000"])
+
+
+#Binning
+bins = np.linspace(min(df["S3+1000"])-1,max(df["S3+1000"])+1,4)
+df['Binned_data'] = pd.cut(df["S3+1000"],bins)
 print(df)
