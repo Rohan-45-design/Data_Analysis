466d7d591dbbfd93f48e000e4e7812e175074a03
Rohan-45-design <149807616+Rohan-45-design@users.noreply.github.com>
2024-08-11 17:33:29 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,3 +1,13 @@
+import numpy as np
 import pandas as pd
-df = pd.read_csv("D:/Tutedude/fruits.csv",header=TRUE)
+df = pd.read_csv("D:/Tutedude/fruits.csv")
+print(df)
+print(df.describe())
+headers = ["Products","S1","S2","S3","S4","S5"]
+df.columns = headers
+df = df.drop(['S4','S5'],axis=1)
+df = df.dropna(subset=['S1','S2','S3'],axis=0)
+df = df.replace(np.nan,22)
+df["S3"]+=1000
+df=df.rename(columns={"S3":"S3+1000"})
 print(df)
